258c91753190d649ffc6811c4aaed0d4ef3e2da4
Vladosik337 <vlad.ostapenko.337@gmail.com>
2024-10-08 23:25:12 +0300

    create project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './styles/style.scss';
+
+function App() {
+    return (
+        <div className="min-h-full flex flex-col items-center justify-center overflow-clip h-screen">
+
+        </div>
+    );
+}
+
+export default App;
