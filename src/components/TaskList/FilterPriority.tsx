7cd47283f060342b650bfc3a860debdbe339d33d
Vladosik337 <vlad.ostapenko.337@gmail.com>
2024-10-24 21:20:30 +0300

    [FEAT] add function handleFilterChange and add styles

--- src/components/TaskList/FilterPriority.tsx
+++ src/components/TaskList/FilterPriority.tsx
@@ -5,24 +5,45 @@ import {filterTaskPriority} from "../../features/taskListsSlice.ts";
 
 interface FilterPriorityProps {
     listId: string;
-    taskIds: string[]; // Змінено на taskIds
+    onFilterChange: (priority: string) => void;
 }
 
-const FilterPriority: React.FC<FilterPriorityProps> = ({listId, taskIds}) => {
+const FilterPriority: React.FC<FilterPriorityProps> = ({listId, onFilterChange}) => {
     const dispatch: AppDispatch = useDispatch();
 
-    const handleFilterChange = (valuePriority: string) => {
-        if (taskIds && taskIds.length > 0) {
-            dispatch(filterTaskPriority({listId, taskIds, valuePriority}));
-        }
+    const handleFilterChange = (priority: string) => {
+        onFilterChange(priority);
+        dispatch(filterTaskPriority({listId, valuePriority: priority}));
     };
 
     return (
-        <div className={'flex space-x-4'}>
-            <button onClick={() => handleFilterChange('high')}>High</button>
-            <button onClick={() => handleFilterChange('medium')}>Medium</button>
-            <button onClick={() => handleFilterChange('low')}>Low</button>
-            <button onClick={() => handleFilterChange('All')}>All</button>
+        <div className="flex flex-col items-center mt-4">
+            <div className="flex space-x-4">
+                <button
+                    onClick={() => handleFilterChange('high')}
+                    className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded transition-colors duration-200 ease-in-out"
+                >
+                    High
+                </button>
+                <button
+                    onClick={() => handleFilterChange('medium')}
+                    className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded transition-colors duration-200 ease-in-out"
+                >
+                    Medium
+                </button>
+                <button
+                    onClick={() => handleFilterChange('low')}
+                    className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded transition-colors duration-200 ease-in-out"
+                >
+                    Low
+                </button>
+                <button
+                    onClick={() => handleFilterChange('All')}
+                    className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded transition-colors duration-200 ease-in-out"
+                >
+                    All
+                </button>
+            </div>
         </div>
     );
 };
